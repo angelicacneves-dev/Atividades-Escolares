80e96b4f49760ea860f3f287cfdb1ec744b4e424
angelicacneves-dev <angelica.c.neves@aluno.senai.br>
2026-02-20 15:25:59 -0300

    Código em Python

---
+++ Projeto02/AtividadeSLN/Atividade/Banco.py
@@ -0,0 +1,25 @@
+﻿class Banco:
+    def __init__(self):
+        self._nome = ""
+        self._codigo_bacen = ""
+
+    @property
+    def nome(self):
+        # Retorna o nome sempre em maiúsculas
+        return self._nome.upper()
+
+    @nome.setter
+    def nome(self, value):
+        self._nome = value
+
+    @property
+    def codigo_bacen(self):
+        return self._codigo_bacen
+
+    @codigo_bacen.setter
+    def codigo_bacen(self, value):
+        # Valida se não é nulo ou vazio
+        if value and value.strip():
+            self._codigo_bacen = value
+        else:
+            print("Erro: O Código BACEN não pode ser vazio.")
---
+++ Projeto02/AtividadeSLN/Atividade/Transacao.py
@@ -0,0 +1,19 @@
+﻿from datetime import datetime
+
+class Transacao:
+    def __init__(self):
+        self._valor = 0.0
+        self.data = datetime.now()
+        self.tipo = ""
+
+    @property
+    def valor(self):
+        return self._valor
+
+    @valor.setter
+    def valor(self, value):
+        # O valor deve ser obrigatoriamente maior que zero
+        if value > 0:
+            self._valor = value
+        else:
+            print("Erro: O valor da transaçăo deve ser positivo.")
