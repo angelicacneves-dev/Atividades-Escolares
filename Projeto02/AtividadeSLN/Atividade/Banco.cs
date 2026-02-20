f557bb5a4761aa778ebd51246f7367c6cd75623a
angelicacneves-dev <angelica.c.neves@aluno.senai.br>
2026-02-20 15:22:13 -0300

    Projeto 02 Fase C

---
+++ Projeto02/AtividadeSLN/Atividade/Banco.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade
+{
+    internal class Banco
+    {
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome.ToUpper(); }
+            set { _nome = value; }
+        }
+
+
+        private string _codigoBACEN;
+        public string CodigoBACEN
+        {
+            get { return _codigoBACEN; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    _codigoBACEN = value;
+                else
+                    Console.WriteLine("Erro: O Código BACEN não pode ser vazio.");
+            }
+        }
+    }
+}
---
+++ Projeto02/AtividadeSLN/Atividade/Transacao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto02
+{
+    internal class Transacao
+    {
+        public DateTime Data { get; set; }
+
+        public string Tipo { get; set; }
+
+        private decimal _valor;
+
+        public decimal Valor
+        {
+            get { return _valor; }
+            set
+            {
+                if (value > 0)
+                    _valor = value;
+                else
+                    Console.WriteLine("Erro: O valor da transação deve ser positivo.");
+            }
+        }
+    }
+}
